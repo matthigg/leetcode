f4053e20ea195b4b9009da042c6dcf7cbdceaf97
matthigg <mhh129@gmail.com>
2019-09-04 15:12:33 -0400

    Update problems

--- Minimum-Window-Substring.py
+++ Minimum-Window-Substring.py
@@ -17,56 +17,68 @@ class MinimumWindowSubstring:
     self.input = input
     self.output_expected = output_expected
 
-# ========== Brute Force ======================================================
+# ========== Sliding Window ===================================================
+# In any sliding window based problem we have two pointers. One right pointer 
+# whose job is to expand the current window and then we have the left pointer 
+# whose job is to contract a given window. At any point in time only one of 
+# these pointers move and the other one remains fixed.
+#
+# The solution is pretty intuitive. We keep expanding the window by moving the 
+# right pointer. When the window has all the desired characters, we contract 
+# (if possible) and save the smallest window till now.
+#
+# The answer is the smallest desirable window.
 # 
-# 
-# Time complexity:
+# Time complexity: O(n) - we traverse the 's' string with 2 pointers, and the 't'
+# string once when we create a Counter dictionary.
 # 
 # Space complexity:
 
-  def bruteForce(self):
+  def slidingWindow(self):
     input, output_expected = self.input, self.output_expected
     result = ''
     s, t = input[0], input[1]
 
-    import sys
-    min_size = sys.maxsize
-    L = 0
-    R = 0
+    # If 's' == 't', then 's' is the smallest string that contains all of the 
+    # characters in 't' and we can return
+    if s == t:
+      result = s
+      print(result == output_expected, output_expected, result)
+      return
+    if len(s) < len(t):
+      result = 'no_substring_found'
+      print(result == output_expected, output_expected, result)
+      return
 
-    t_set = set()
-    def regenerate_t_set():
-      for i in t:
-        t_set.add(i)
-    regenerate_t_set()
+    # Create a Counter dictionary
+    from collections import Counter
+    tcount = Counter(t)
 
-    recording = False
-    current_string = ''
+    # Sliding window
+    R = L = 0
+    substring_found = False
+    substring = ()
+    min_substring = (len(s), 0, 0)
     while R < len(s):
+      if s[R] in tcount:
+        tcount[s[R]] -= 1
 
-      if s[R] in t_set:
-        t_set.remove(s[R])
-        recording = True
+        while all(value <= 0 for value in tcount.values()):
+          substring = (R - L, L, R)
+          if substring[0] < min_substring[0]:
+            substring_found = True
+            min_substring = substring
+          if s[L] in tcount:
+            tcount[s[L]] += 1
+          L += 1
+
       R += 1
 
-      if len(t_set) == 0:
-        print(min_size, R - L)
-        min_size = min(min_size, R - L)
-        L = R
-        regenerate_t_set()
-        recording = False
-
-      if recording == False:
-        L = R
-
-    # print(t_set, min_size)
-
-
-
-
-
-    # print(s, t, output_expected)
-    # print(result = output_expected, output_expected, result)
+    # Return results
+    result = s[min_substring[1]:min_substring[2] + 1]
+    if substring_found == False:
+      result = 'no_substring_found'
+    print(result == output_expected, output_expected, result)
 
 # ========== Tests ============================================================
 
@@ -79,8 +91,8 @@ def test(test_file, fn):
       else:
         output_expected = line.strip('\n')
         mws = MinimumWindowSubstring(input, output_expected)
-        if fn == 'bruteForce':
-          mws.bruteForce()
+        if fn == 'slidingWindow':
+          mws.slidingWindow()
 
 # ========== Command Line Arguments ===========================================
 
